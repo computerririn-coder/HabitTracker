331b50550a5412e92f0e1a6b90bd0c702cef0a6d
Your Name <your@email.com>
2026-01-21 10:03:36 +0800

    FIxing files to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@
 
 import './App.css'
 import NavigationBar from './assets/Components/NavigationBar'
-import MainSection from './assets/Components/Section/MainSection'
 import Footer from './assets/Components/Footer'
 import TasksBar from './assets/Components/Section/TasksBar.tsx'
-import AddNewTab from './assets/Components/Section/addNewTab.tsx'
-import Testing from './assets/Components/Section/Testing.tsx'
-import EditHotkey from './assets/Components/Section/editHotkey.tsx'
 import Achievements from './assets/Components/Section/Achievements.tsx'
 import { useComponentVisibility } from './assets/Components/Section/store.ts'
 
